8b94976dc02b6faa5b309bf25b10ecbd03268084
texasanna <antony@4ft.de>
2021-03-05 09:12:45 +0100

    new videos

--- video/video.js
+++ video/video.js
@@ -1,25 +1,25 @@
-const videoPlayer = document.getElementById('videoPlayer')
-videoPlayer.controls = false
-videoPlayer.muted = true
-console.log(videoPlayer)
+const videoPlayer = document.getElementById("videoPlayer");
+videoPlayer.controls = false;
+videoPlayer.muted = true;
+console.log(videoPlayer);
 
 videoPlayer.setVideo = () => {
-    videoPlayer.controls = false
-    videoPlayer.muted = true
-    switch(map.currentMarker) {
-        case 1:
-            videoPlayer.src = "soundvisualizer/final/hiking_animated.mp4"
-            break;
-        case 2:
-            videoPlayer.src = "soundvisualizer/final/idea_animated.mp4"
-            break;
-        case 3:
-            videoPlayer.src = "soundvisualizer/final/hiking_animated.mp4"
-            break;
-        case 4:
-            videoPlayer.src = "soundvisualizer/final/intro_animated.mp4"
-            break;
-        default: 
-            return
-    }
-}
+  videoPlayer.controls = false;
+  videoPlayer.muted = true;
+  switch (map.currentMarker) {
+    case 1:
+      videoPlayer.src = "soundvisualizer/final/hiking_animated.mp4";
+      break;
+    case 2:
+      videoPlayer.src = "soundvisualizer/final/idea_animated.mp4";
+      break;
+    case 3:
+      videoPlayer.src = "soundvisualizer/final/hiking_animated.mp4";
+      break;
+    case 4:
+      videoPlayer.src = "soundvisualizer/final/added-value.mp4";
+      break;
+    default:
+      return;
+  }
+};
